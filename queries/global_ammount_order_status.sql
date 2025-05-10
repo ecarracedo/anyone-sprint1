b42fc496dea21ee277c69a63041978767ec55391
Emiliano Carracedo <ecarracedo@gmail.com>
2025-05-09 22:18:10 -0300

    Update Repo

--- queries/global_ammount_order_status.sql
+++ queries/global_ammount_order_status.sql
@@ -2,7 +2,7 @@
 -- Ammount. The first one will have the different order status classes and the
 -- second one the total ammount of each.
 SELECT oo.order_status AS order_status,
-COUNT(oop.payment_value) AS Amount
+COUNT(oop.payment_value) AS Ammount
 FROM (SELECT * FROM olist_orders oo WHERE oo.order_status IS NOT NULL ) oo
 	JOIN olist_order_payments oop ON oo.order_id = oop.order_id
 GROUP BY oo.order_status
--- queries/real_vs_estimated_delivered_time.sql
+++ queries/real_vs_estimated_delivered_time.sql
@@ -16,20 +16,39 @@
 -- 3. Take distinct order_id.
 
 SELECT
-    c.customer_state AS State,
-    CAST(
-        AVG(
-            julianday(DATE(o.order_estimated_delivery_date)) - julianday(DATE(o.order_delivered_customer_date))
-        ) AS INTEGER
-        ) AS Delivery_Difference
+	month_no,
+	CASE 
+		WHEN month_no = '01' THEN 'Jan'
+		WHEN month_no = '02' THEN 'Feb'
+		WHEN month_no = '03' THEN 'Mar'
+		WHEN month_no = '04' THEN 'Apr'
+		WHEN month_no = '05' THEN 'May'
+		WHEN month_no = '06' THEN 'Jun'
+		WHEN month_no = '07' THEN 'Jul'
+		WHEN month_no = '08' THEN 'Aug'
+		WHEN month_no = '09' THEN 'Sep'
+		WHEN month_no = '10' THEN 'Oct'
+		WHEN month_no = '11' THEN 'Nov'
+		WHEN month_no = '12' THEN 'Dec'
+	END AS month,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2016' THEN real_time END), 6),'NaN') as Year2016_real_time,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2017' THEN real_time END), 6),'NaN') as Year2017_real_time,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2018' THEN real_time END), 6),'NaN') as Year2018_real_time,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2016' THEN estimated_time END), 6),'NaN') as Year2016_estimated_time,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2017' THEN estimated_time END), 6),'NaN') as Year2017_estimated_time,
+	COALESCE(ROUND(AVG( CASE WHEN year == '2018' THEN estimated_time END), 6),'NaN') as Year2018_estimated_time
 FROM
-    olist_orders o
-JOIN
-    olist_customers c ON o.customer_id = c.customer_id
-WHERE
-    o.order_status = 'delivered'
-    AND o.order_delivered_customer_date IS NOT NULL
-GROUP BY
-    c.customer_state
-ORDER BY
-    Delivery_Difference;
+	(
+		SELECT 
+			order_id,
+			(JULIANDAY(order_delivered_customer_date)-JULIANDAY(order_purchase_timestamp)) AS real_time,
+			(JULIANDAY(order_estimated_delivery_date)-JULIANDAY(order_purchase_timestamp)) AS estimated_time,
+			STRFTIME('%m', order_purchase_timestamp) AS month_no,
+			STRFTIME('%Y', order_purchase_timestamp) AS year
+		FROM olist_orders oo
+		WHERE
+			order_delivered_customer_date IS NOT NULL 
+			AND order_status = 'delivered'
+	)
+	GROUP BY month_no
+	
